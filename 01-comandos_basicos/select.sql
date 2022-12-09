b00144be673c816d518d19cb47e61f3e238e7f6a
razevedo1994 <razevedo.contato@gmail.com>
2022-12-09 18:25:13 -0300

    Basic comands: select

---
+++ 01-comandos_basicos/select.sql
@@ -0,0 +1,13 @@
+-- Liste os e-mails dos clientes da tabela sales.customers
+SELECT email
+FROM sales.customers
+
+
+-- Liste os emails e nomes dos clientes da tabela sales.customers
+SELECT email, first_name, last_name
+FROM sales.customers
+
+
+-- Liste todos as informações dos clientes da tabela sales.customers
+SELECT *
+FROM sales.customers
